257cb5ab81f47b3af223868138ee271fcc738ae3
anm-jubaer <anm-jubaer@digigate360.com>
2022-11-15 19:48:21 +0600

    IService created

---
+++ src/main/java/com/jb/genericCRUD/Generics/service/IService.java
@@ -0,0 +1,22 @@
+package com.jb.genericCRUD.Generics.service;
+
+import com.jb.genericCRUD.Generics.entity.GenericEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IService <E extends GenericEntity>{
+
+    <T> T getSingle(Long id);
+
+    E findById(Long id);
+
+    E create(E e);
+
+    E update(E e, Long id);
+
+    E saveItem(E e);
+
+    Page<E> getAll(Pageable pageable);
+
+    <T> E findByName(T t);
+}
