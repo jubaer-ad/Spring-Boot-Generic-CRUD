ac3aa9aeacc3574a7e278303e04951b1d3f69403
anm-jubaer <anm-jubaer@digigate360.com>
2022-11-15 19:25:02 +0600

    updated book entity

--- src/main/java/com/jb/genericCRUD/entity/Book.java
+++ src/main/java/com/jb/genericCRUD/entity/Book.java
@@ -3,7 +3,20 @@ package com.jb.genericCRUD.entity;
 import com.jb.genericCRUD.Generics.entity.GenericDomainBasedEntity;
 import com.jb.genericCRUD.Generics.entity.GenericEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Table(name = "book")
 public class Book extends GenericDomainBasedEntity implements GenericEntity<Book> {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "description")
+    private String description;
     @Override
     public void update(Book source) {
 
--- src/main/java/com/jb/genericCRUD/repository/BookRepository.java
+++ src/main/java/com/jb/genericCRUD/repository/BookRepository.java
@@ -2,6 +2,19 @@ package com.jb.genericCRUD.repository;
 
 import com.jb.genericCRUD.Generics.repository.GenericRepository;
 import com.jb.genericCRUD.entity.Book;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.Set;
 
 public interface BookRepository extends GenericRepository<Book> {
+
+    @Query("select b from Book b where b.name = :name")
+    Optional<Book> findByName(@Param("name") String name);
+
+    @Query(" select p.name from Position p where p.isActive= true")
+    Set<String> getAllNames();
+
+    Set<Book> findAllByIsActiveTrue();
 }
